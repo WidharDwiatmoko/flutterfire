4ec5cf7419ae1775fbe4e7c4b25505da8441fb0d
WidharDwiatmoko <widhardwi23@gmail.com>
2021-05-07 08:30:13 +0700

    Update firebase_analytics.dart
    remove screen_view

--- packages/firebase_analytics/firebase_analytics/lib/firebase_analytics.dart
+++ packages/firebase_analytics/firebase_analytics/lib/firebase_analytics.dart
@@ -915,7 +915,7 @@ const List<String> _reservedEventNames = <String>[
   'notification_open',
   'notification_receive',
   'os_update',
-  'screen_view',
+  //'screen_view',
   'session_start',
   'user_engagement',
 ];
